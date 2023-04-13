c58b4037df8952d3ee68aa363e4a5db5e6700d06
Filipe Alves <lipeqa@gmail.com>
2023-04-13 09:28:50 -0300

    feat: usa os dados fakes e deixa conteudo dinamico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Novo Homem de Ferro Anuciado.",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title":"saiu o novo filme2",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },{
+    "id":"3",
+    "title":"saiu o novo filme3",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"4",
+    "title":"saiu o novo filme4",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from "../../data/dataFake"
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,10 +11,25 @@ export class ContentComponent implements OnInit {
   photoCover: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrhwwH8uRcQYJahZ1fcOHWHstnIzoxeWWJTA&usqp=CAU"
   contentTitle: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit."
   contentDescription: string = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam aspernatur quae dignissimos possimus modi, perspiciatis."
+  private id:string | null = "0"
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
-  constructor() { }
   ngOnInit(): void {
-
+    this.route.paramMap.subscribe(value =>
+     this.id = value.get("id")
+    )
+   this.setValuesToComponent(this.id)
   }
-
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+   this.contentTitle = result.title
+   this.contentDescription = result.description
+   this.photoCover = result.photoCover
+  }
 }
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
