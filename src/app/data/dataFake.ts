b57b8b0722fa38cdd0ce35a2435a97a5eff06deb
Filipe Alves <lipeqa@gmail.com>
2023-04-17 21:16:41 -0300

    ajustes de informações

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,25 +1,34 @@
+
 export const dataFake = [
   {
-    "id":"1",
-    "title":"Novo Homem de Ferro Anuciado.",
-    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
-    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "1",
+    "title": "Desafio Front-end Flexpag",
+    "description": "Eu enquanto Flexpag, desejo que seja criado uma um simulador de precificação para venda de veículo. O simulador consinste em que eu possa selecionar o tipo do veículo, Marca do veículo, Modelo do veículo, e ano, e que possa inserir o valor da venda do veículo que desejo vender o mesmo, e o simulador me informar todos os dados do veículo que quero vender, e se o valor em que adicionei está abaixo, acima, ou na média de preço do mercado de veículos. Também exiba o percentual do valor que adicionei para venda em relação a tabela do mercado.",
+    "photoCover": "../../assets/flexpag.png",
+    "gitHub": "https://github.com/lipeqalves/flexpag-desafio-frontend",
+    "paginaWeb": "https://flexpag-desafio-frontend.vercel.app/"
   },
   {
-    "id":"2",
-    "title":"saiu o novo filme2",
-    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
-    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  },{
-    "id":"3",
-    "title":"saiu o novo filme3",
-    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
-    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "2",
+    "title": "Formulário de Inscrição",
+    "description": "Primeiro projeto do modulo I do curso de Web Dev da Resilia Educação. O objetivo deste desafio é criar uma página de cadastro para o site da resilia, utilizando HTML, CSS com um layout responsivo",
+    "photoCover": "../../assets/formulario.png",
+    "gitHub": "https://github.com/lipeqalves/formularioInscricao",
+    "paginaWeb": "https://formulario-inscricao-mu.vercel.app/"
+  }, {
+    "id": "3",
+    "title": "Projeto de encerramento do módulo 5: Desenvolvimento de site",
+    "description": "Desenvolvemos um site para a nossa startup Alcance utilizando a biblioteca React.js e endpoints da API que criamos anteriormente com Node.Js e Express, usamos também gerenciamentos de rotas com a biblioteca react-router-dom e por fim contendo funcionalidades básicas do CRUD.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+    "gitHub": "https://github.com/lipeqalves/alcance-page",
+    "paginaWeb": "https://alcance.vercel.app/"
   },
   {
-    "id":"4",
-    "title":"saiu o novo filme4",
-    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa ullamiusto vel accusantium error.",
-    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "4",
+    "title": "Projeto MKS: Desenvolvimento de uma página de compras",
+    "description": "Foi desenvolvido um site simulando a pagina de compras de uma loja, onde é posivel adicionar e remover os itens do carrinho usando uma API que lista varios produtos.",
+    "photoCover": "../../assets/mks.png",
+    "gitHub": "https://github.com/lipeqalves/challenge-mks",
+    "paginaWeb": "https://challenge-mks-61n4-3jg10mouk-lipeqalves.vercel.app/"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {dataFake} from "../../data/dataFake"
+import { dataFake } from "../../data/dataFake"
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -8,25 +8,30 @@ import {dataFake} from "../../data/dataFake"
 })
 export class ContentComponent implements OnInit {
 
-  photoCover: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrhwwH8uRcQYJahZ1fcOHWHstnIzoxeWWJTA&usqp=CAU"
-  contentTitle: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-  contentDescription: string = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam aspernatur quae dignissimos possimus modi, perspiciatis."
-  private id:string | null = "0"
+  photoCover: string = ""
+  contentTitle: string = ""
+  contentDescription: string = ""
+  gitHub: string = ""
+  pageWeb: string = ""
+
+  private id: string | null = "0"
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(value =>
-     this.id = value.get("id")
+      this.id = value.get("id")
     )
-   this.setValuesToComponent(this.id)
+    this.setValuesToComponent(this.id)
   }
-  setValuesToComponent(id:string | null){
+  setValuesToComponent(id: string | null) {
     const result = dataFake.filter(article => article.id == id)[0]
-   this.contentTitle = result.title
-   this.contentDescription = result.description
-   this.photoCover = result.photoCover
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+    this.gitHub = result.gitHub
+    this.pageWeb = result.paginaWeb
   }
 }
 function ngOnInit() {
